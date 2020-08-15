0d5c69e52054751cafcac48b591510bff45e8ef2
Zach Holcomb <zachholcombmusic@gmail.com>
2020-08-15 11:39:51 -0600

    Configures emailjs for contact form

---
+++ javascript/email.js
@@ -0,0 +1,21 @@
+$("#contact-form").on("submit", function(event) {
+  event.preventDefault(); 
+  console.log(this);
+  let formData = new FormData(this);
+
+  formData.append('service_id', 'gmail');
+  formData.append('template_id', 'template_FCwYklu8');
+  formData.append('user_id', 'user_ZrkMNdElPrhCXtg19YAVZ');
+  
+  $.ajax('https://api.emailjs.com/api/v1.0/email/send-form', {
+    type: 'POST',
+    data: formData,
+    contentType: false,
+    processData: false
+  }).done(function() {
+    alert('Thanks for contacting me! I will get back to you ASAP!');
+  }).fail(function(error) {
+    console.log(error);
+    alert('Oops...' + JSON.stringify(error));
+  });
+});
